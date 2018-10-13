0bd0d196ca6ed005b2863f913514eb39bd7b7c88
David Tamayo <jtamayoh@uniminuto.edu.co>
2018-10-13 18:25:04 -0500

    Update CashRegister.rb

--- CashRegister.rb
+++ CashRegister.rb
@@ -1,3 +1,40 @@
+load 'Client.rb'
+
+
 class CashRegister
-  
+    def initialize
+        @available = true
+      end
+    
+      # @client = Client.new
+    
+      attr_reader :client
+      attr_accessor :available
+    
+      def serviceTime
+        #de 4 a 25 minutos
+        random = Random.new
+        random.rand(4..25)
+      end
+    
+      def customerArrives client, tArrive
+        # puts "Atender al cliente: #{client.representation}"
+        # puts "------------"
+        # puts "Atendiendo"
+        @client = client
+        # puts "Cliente: #{@client.representation}"
+        @available = false
+        @timeArrive = tArrive
+        # puts "arribo: #{@timeArrive}"
+        @serviceTime = serviceTime
+        # puts "tServicio: #{@serviceTime}"
+      end
+    
+      def serveCustomer timeSimulacion
+        unless @available
+          if timeSimulacion == @timeArrive + @serviceTime
+            @available = true
+          end
+        end
+      end
 end
