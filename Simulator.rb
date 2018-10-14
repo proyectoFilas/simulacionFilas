a730eceac8b34ff026e5dd44a8de511e422cfbbd
Christian Torres <cctorresr@unal.edu.co>
2018-10-13 20:17:40 -0500

    Se agrega método que genera clientes nuevos

--- Simulator.rb
+++ Simulator.rb
@@ -10,4 +10,17 @@ class Simulator
 
   def run
   end
+
+  def newClients
+    #Llegan entre 0-5 clientes
+    random = Random.new
+    numClients = random.rand(0..5)
+    # Array.new( numClients, Client.new(@time))
+    clients = Array.new
+    for n in (1..numClients)
+      clients << Client.new(@time)
+    end
+    clients
+  end
+
 end
